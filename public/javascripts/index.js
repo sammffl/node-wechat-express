384090beb48cdac0adbb722d766e5f33e995409b
sammffl <samfec@163.com>
2016-11-30 11:48:48 +0800

    add tag

---
+++ public/javascripts/index.js
@@ -0,0 +1,21 @@
+// window.addEventListener("devicemotion", function (event) {
+//     var eventacceleration = event.acceleration;
+//     document.querySelector('#devicemotion').innerHTML = "acceleration:<br>" +
+//         eventacceleration.x + "<br>" +
+//         eventacceleration.y + "<br>" +
+//         eventacceleration.z
+// }, false);
+
+
+tick(function () {
+
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    
+    document.querySelector('#devicemotion').innerHTML = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+});
